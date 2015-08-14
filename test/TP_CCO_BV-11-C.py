515d0aa8c9ad0daf7f8517b5ad0744434f719584
Guan-Zhong Huang <imprazaguy@gmail.com>
2015-08-14 18:25:39 +0800

    Add more test scripts.

---
+++ test/TP_CCO_BV-11-C.py
@@ -0,0 +1,44 @@
+# Test LE ACL data transmission from master to slave.
+
+import bluetool
+from bluetool.core import HCICoordinator, HCIWorker, HCIWorkerProxy, HCITask, LEHelper
+from bluetool.bluez import ba2str
+from bluetool.error import HCICommandError, TestError, HCITimeoutError
+import bluetool.bluez as bluez
+import bluetool.command as btcmd
+import bluetool.event as btevt
+from bluetool.data import HCIACLData
+from bluetool.utils import bytes2str, htole16
+
+CONN_TIMEOUT_MS = 10000
+
+class LEMaster(HCIWorker):
+    def __init__(self, hci_sock, coord, pipe):
+        super(LEMaster, self).__init__(hci_sock, coord, pipe)
+
+    def main(self):
+        helper = LEHelper(self.sock)
+
+        try:
+            helper.reset()
+            cmd = btcmd.HCILEWriteSuggestedDefaultDataLength(251, (251+14)*8)
+            helper.send_hci_cmd_wait_cmd_complt_check_status(cmd)
+            cmd = btcmd.HCILEReadSuggestedDefaultDataLength()
+            helper.send_hci_cmd_wait_cmd_complt_check_status(cmd)
+        except HCICommandError as err:
+            self.log.warning('cannot reset', exc_info=True)
+            return
+
+class LETester(HCICoordinator):
+    def __init__(self):
+        super(LETester, self).__init__()
+        self.worker.append(HCIWorkerProxy(0, self, LEMaster))
+
+    def main(self):
+        pass
+
+if __name__ == "__main__":
+    bluetool.log_to_stream()
+    tester = LETester()
+    tester.run()
+
