f24fb2e5b30d5d01bf76ce988fec8c8914afc072
Kuan-Chung Huang <imprazaguy@gmail.com>
2017-10-05 22:16:49 +0800

    Tweak codes and format codes

--- bluetool/error.py
+++ bluetool/error.py
@@ -10,40 +10,46 @@ class Error(Exception):
 class HCIError(Error):
     def __init__(self, msg=''):
         super(HCIError, self).__init__(msg)
-        self.msg = msg
+
 
 class HCIParseError(HCIError):
     def __init__(self, msg):
         super(HCIParseError, self).__init__(msg)
 
+
 class HCIEventNotImplementedError(HCIError):
     def __init__(self, evt_code):
         self.evt_code = evt_code
         super(HCIEventNotImplementedError, self).__init__(
-                '{}: code: 0x{:02X}'.format(self.__class__.__name__, evt_code))
+            '{}: code: 0x{:02X}'.format(self.__class__.__name__, evt_code))
+
 
 class HCICommandCompleteEventNotImplementedError(HCIError):
     def __init__(self, opcode):
         self.opcode = opcode
         super(HCICommandCompleteEventNotImplementedError, self).__init__(
-                '{}: opcode: 0x{:04X}'.format(self.__class__.__name__, opcode))
+            '{}: opcode: 0x{:04X}'.format(self.__class__.__name__, opcode))
+
 
 class HCILEEventNotImplementedError(HCIError):
     def __init__(self, subevt_code):
         self.subevt_code = subevt_code
         super(HCILEEventNotImplementedError, self).__init__(
-                '{}: subevt_code: 0x{:02X}'.format(self.__class__.__name__, subevt_code))
+            '{}: subevt_code: 0x{:02X}'.format(
+                self.__class__.__name__, subevt_code))
+
 
 class HCITimeoutError(HCIError):
-    def __init__(self):
-        super(HCITimeoutError, self).__init__()
+    def __init__(self, msg=''):
+        super(HCITimeoutError, self).__init__(msg)
+
 
 class HCICommandError(HCIError):
     def __init__(self, hci_evt):
         super(HCICommandError, self).__init__(
-                '{} failure: opcode: 0x{:04x}, status: 0x{:02x}'.format(
-                    hci_evt.__class__.__name__, hci_evt.cmd_opcode,
-                    hci_evt.status))
+            '{} failure: opcode: 0x{:04x}, status: 0x{:02x}'.format(
+                hci_evt.__class__.__name__, hci_evt.cmd_opcode,
+                hci_evt.status))
 
 
 class HCIInvalidCommandParametersError(HCIError):
